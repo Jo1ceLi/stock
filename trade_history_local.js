10194ef0e97a5014c65d443c4a02d7b2b4bc9804
Jo1ceLi <ms0615122@gmail.com>
2020-12-16 20:25:58 +0800

    Integrate mongo DB models Fixes #20

--- trade_history_local.js
+++ trade_history_local.js
@@ -2,13 +2,14 @@ var mongoose = require('mongoose')
 var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+var positionModel = require('./models/Position')
+var historicalTradeDataModel = require('./models/HistoricalTradeData')
 
 var app = express()
- 
+// use cors
 app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 
@@ -19,14 +20,14 @@ var svr;
 var db;
 var port;
 if(process.env.ENV==="DEV"){
-    console.log('Dev environment configuration is preparing')
+    // console.log('Dev environment configuration is preparing')
     user = process.env.DEV_DB_USER;
     pw = process.env.DEV_DB_PASSWORD;
     svr = process.env.DEV_DB_SERVER;
     db = process.env.DEV_DB_NAME;
     port = process.env.DEV_PORT;
 }else if(process.env.ENV==="PROD"){
-    console.log('Production environment configuration is preparing')
+    // console.log('Production environment configuration is preparing')
     user = process.env.PROD_DB_USER;
     pw = process.env.PROD_DB_PASSWORD;
     svr = process.env.PROD_DB_SERVER;
@@ -36,114 +37,94 @@ if(process.env.ENV==="DEV"){
     console.log('Can not recognize this enviroment')
 }
 
+var Position = positionModel.PositionModel;
+var HistoricalTradeData = historicalTradeDataModel.HistoricalTradeDataModel;
 
-// console.log(`mongodb://${user}:${pw}@${svr}:${port}/${db}`)
-
-
-// mongoose.connect(`mongodb://${user}:${pw}@${svr}:${port}/${db}`, {useUnifiedTopology: true, useNewUrlParser: true })
-// mongoose.connect(`mongodb://jo1ce:Alan0114@mongodb:27017/trade`, {useUnifiedTopology: true, useNewUrlParser: true })
-
-
-const historicalTradeDataModel = mongoose.model('historical_trade_data', new mongoose.Schema({symbol: String, price: Number, amount: Number, date: Date}));
-const positionDataModel = mongoose.model('position', new mongoose.Schema({symbol: String, avgPrice: Number, amount: Number}));
-
-// historicalTradeDataModel.insertMany({symbol: 'PLTR', price: 21.50, amount: 5, date: new Date('2020-12-03')});
 
 // =====  Read  ======
 app.get('/api/trade/:id', async(req, res)=>{
-    var docs = await historicalTradeDataModel.findById(req.params.id)
+    var docs = await HistoricalTradeData.findById(req.params.id)
     .then(console.log('found one data'))
     res.json(docs)
 })
 
 app.get('/api/trade', async (req, res)=>{
-    var docs = await historicalTradeDataModel.find()
+    var docs = await HistoricalTradeData.find()
     .then()
 
     res.json(docs)
 })
 
 app.get('/api/positions', async (req, res)=>{
-    var docs = await positionDataModel.find()
+    var docs = await Position.find()
     .then()
     res.json(docs)
 })
 
 // ====== Create ======
 app.post('/api/trade', async(req, res)=>{
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    // try{
-        const opts = { session, new: true };
-        var trade_insertManyRes = await historicalTradeDataModel.insertMany({symbol: req.body.symbol, price: req.body.price, amount: req.body.amount, date: req.body.date}, opts)
-        // .then(console.log('insert success'))
+    // const session = await mongoose.startSession();
+    // session.startTransaction();
+    //     const opts = { session, new: true };
+        var trade_insertManyRes = await HistoricalTradeData.insertMany({symbol: req.body.symbol, price: req.body.price, amount: req.body.amount, date: req.body.date})
+        
         console.log("tradeInsertManyRes",trade_insertManyRes);
-        var doc = await positionDataModel.find({symbol: req.body.symbol}, opts).exec()
+        var doc = await Position.find({symbol: req.body.symbol}).exec()
 
         if( doc.length != 0 ){
             filter = { symbol: req.body.symbol}
-            update = { avgPrice: ((doc[0].amount*doc[0].avgPrice)+(req.body.price*req.body.amount))/(req.body.amount+doc[0].amount),
+            update = { cost: ((doc[0].amount*doc[0].cost)+(req.body.price*req.body.amount))/(req.body.amount+doc[0].amount),
                        amount: doc[0].amount + req.body.amount }
-            await positionDataModel.findOneAndUpdate(filter, update, opts).exec()
+            await Position.findOneAndUpdate(filter, update).exec()
             console.log('update a existing symbol successful')
         }
-        // // new symbol for position
         else{
-            await positionDataModel.insertMany({symbol: req.body.symbol, avgPrice: req.body.price, amount: req.body.amount}, opts)
+            await Position.insertMany({symbol: req.body.symbol, cost: req.body.price, amount: req.body.amount})
             console.log('insert a new symbol to position')
         }
-        await session.commitTransaction();
-        session.endSession();
         res.send('Adding trade data success');
-    // }
-    // catch(e){
-    //     await session.abortTransaction();
-    //     session.endSession();
-    //     // console.log('Transaction error')
-    //     res.send('Adding trade data fail');
-    //     throw e;
-    // }
-    
+ 
 })
 
 // ======= Update =======
 app.put('/api/trade/:id', async(req, res)=>{
-    var trade_doc = await historicalTradeDataModel.findByIdAndUpdate(req.params.id,
+    var trade_doc = await HistoricalTradeData.findByIdAndUpdate(req.params.id,
         {price: req.body.price, amount: req.body.amount})
     .then(console.log('update success'))
-    console.log(trade_doc.symbol)
-    // console.log(trade_doc)
+    .catch(err=>{
+        res.send('Can not find this data in trade data')
+    })
     res.json(trade_doc)
-    var position_doc = await positionDataModel.find({symbol: trade_doc.symbol}).exec()
+    var position_doc = await Position.find({symbol: trade_doc.symbol}).exec()
     if(position_doc.length === 0){
         console.log('Error! Symbol is not in position data!')
     }else{
-        var new_avgPrice
-        console.log('to update')
-        console.log(position_doc)
-        new_avgPrice = ((position_doc[0].avgPrice * position_doc[0].amount - trade_doc.price * trade_doc.amount + req.body.price * req.body.amount)
+        new_avgPrice = ((position_doc[0].cost * position_doc[0].amount - trade_doc.price * trade_doc.amount + req.body.price * req.body.amount)
                         /(position_doc[0].amount - trade_doc.amount + req.body.amount))
         new_amount = (position_doc[0].amount - trade_doc.amount + req.body.amount)
-        // filter = { symbol: req.body.symbol}
-        // update = { avgPrice: ((doc[0].amount*doc[0].avgPrice)+(req.body.price*req.body.amount))/(req.body.amount+doc[0].amount),
-        //            amount: doc[0].amount + req.body.amount }
-        await positionDataModel.findOneAndUpdate({symbol: trade_doc.symbol}, {avgPrice: new_avgPrice, amount: new_amount}).exec()
+       
+        await Position.findOneAndUpdate({symbol: trade_doc.symbol}, {cost: new_avgPrice, amount: new_amount}).exec()
     }
 })
 // 
 // ======== Delete ========
 app.delete('/api/trade/:id', async (req, res)=> {
     
-    var trade_doc = await historicalTradeDataModel.findByIdAndRemove(req.params.id)
+    var trade_doc = await HistoricalTradeData.findByIdAndRemove(req.params.id)
     .then(console.log('delete success'))
-    var position_doc = await positionDataModel.find({symbol: trade_doc.symbol}).exec()
+    var position_doc = await Position.find({symbol: trade_doc.symbol}).exec()
     if(position_doc.length === 0){
         console.log('Error! Symbol is not in position data!')
     }else{
         
-        new_avgPrice = (( position_doc[0].avgPrice * position_doc[0].amount ) - (trade_doc.price * trade_doc.amount)) / (position_doc[0].amount - trade_doc.amount);
+        new_avgPrice = (( position_doc[0].cost * position_doc[0].amount ) - (trade_doc.price * trade_doc.amount)) / (position_doc[0].amount - trade_doc.amount);
         new_amount = (position_doc[0].amount - trade_doc.amount);
-        await positionDataModel.findOneAndUpdate({symbol: trade_doc.symbol}, {avgPrice: new_avgPrice, amount: new_amount}).exec()
+        console.log('new amount = ',new_amount);
+        if(new_amount != 0){
+            await Position.findOneAndUpdate({symbol: trade_doc.symbol}, {cost: new_avgPrice, amount: new_amount}).exec()
+        }else{
+            await Position.findOneAndDelete({symbol: trade_doc.symbol}).exec();
+        }
     }
     res.send(`delete trade id = ${req.params.id}`)
   });
@@ -156,23 +137,7 @@ app.listen(app_port, ()=>{
 })
 
 async function main(){
-
     const openUrl = `mongodb://${user}:${pw}@${svr}:${port}/${db}`;
-    //const openUrl = "mongodb://jo1ce:Alan0114@localhost:27018/trade";
     await mongoose.connect(openUrl, {useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true}).then(console.log('DB connected!'))
-
-    // await historicalTradeDataModel.find((err, res)=>{
-    //     if(err) console.log(err)
-    //     else console.log(res)
-    // }).catch(err=>{
-    //     console.log(err);
-    // })
-
-
-
-    // await historicalTradeDataModel.insertMany({symbol: 'PLTR', price: 21.50, amount: 5, date: new Date('2020-12-03')});
-    //console.log(result)
-    //mongoose.connection.close()
-
 }
 main();
